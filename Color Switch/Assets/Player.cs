8ea48cca7c845c0772dd3e399c15a92423a4b386
BrianD147 <briandoyle147@gmail.com>
2018-10-01 16:07:19 +0100

    Added color tags, collision detection, and random color generation

--- Color Switch/Assets/Player.cs
+++ Color Switch/Assets/Player.cs
@@ -1,14 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     [SerializeField]
     private float jumpForce;
 
+    private string currentColor;
+
     Rigidbody2D rb;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+
+        SetRandomColor();
+    }
+
+   void SetRandomColor()
+    {
+        int index = Random.range(0, 3);
+
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                break;
+            case 1:
+                currentColor = "Yellow";
+                break;
+            case 2:
+                currentColor = "Magenta";
+                break;
+            case 3:
+                currentColor = "Pink";
+                break;
+        }
     }
 
     void Update () {
@@ -17,4 +43,12 @@ public class Player : MonoBehaviour {
             rb.velocity = Vector2.up * jumpForce;
         }
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Yellow")
+        {
+
+        }
+    }
 }
