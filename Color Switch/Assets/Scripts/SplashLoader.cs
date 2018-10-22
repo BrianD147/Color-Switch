e51c3db2965548a12556b844bb1097f3711e90f2
BrianD147 <briandoyle147@gmail.com>
2018-10-22 23:00:44 +0100

    Added a splash screen and background music

---
+++ Color Switch/Assets/Scripts/SplashLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashLoader : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+
+    IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load the menu screen
+    }
+}
