f5b5cce1d4c6e4556f3adcb05382de3cee293961
BrianD147 <briandoyle147@gmail.com>
2018-10-23 10:42:30 +0100

    Added funtionality to the music on/off button

---
+++ Color Switch/Assets/Scripts/MusicOption.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicOption : MonoBehaviour {
+
+    public static bool MusicIsPlaying = true; // Used to determine if music is playing
+
+    //ublic AudioSource Music;
+
+    public Text buttonText;
+
+    public void MusicToggle()
+    {
+        if (MusicIsPlaying)
+        {
+            //Music.Pause();
+            AudioListener.pause = true;
+            buttonText.text = "Music OFF";
+            MusicIsPlaying = false;
+        } else
+        {
+            //Music.Play();
+            AudioListener.pause = false;
+            buttonText.text = "Music ON";
+            MusicIsPlaying = true;
+        }
+    }
+}
