3f935c10874c00afa70367f89a9a3da0b63d1e5d
BrianD147 <briandoyle147@gmail.com>
2018-10-24 20:10:28 +0100

    Found problems in facebook apk

--- Color Switch/Assets/Scripts/GameOver.cs
+++ Color Switch/Assets/Scripts/GameOver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Facebook.Unity;
 
 public class GameOver : MonoBehaviour {
 
@@ -17,17 +18,51 @@ public class GameOver : MonoBehaviour {
         highScoreText.text = highScore.ToString(); // set on screen highScore to the highScore value
         score = PlayerPrefs.GetInt("Score");
         scoreText.text = score.ToString();
+
+        if (!FB.IsInitialized)
+        {
+            FB.Init(InitCallBack);
+        }
+        
+    }
+
+    void InitCallBack()
+    {
+        Debug.Log("FB has been initialised");
     }
 
     public void NavToMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2); // Load the menu screen
-
     }
 
     public void NewGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); // Load a new game screen
+    }
 
+    public void Login()
+    {
+        if (!FB.IsLoggedIn)
+        {
+            FB.LogInWithReadPermissions(new List<string> { "user_friends" }, LoginCallBack); // Attempt login with read permissions
+        }
+    }
+
+    void LoginCallBack(ILoginResult result)
+    {
+        if (result.Error == null)
+        {
+            Debug.Log("FB has logged in."); // Login success
+        } else
+        {
+            Debug.Log("Error during login: " + result.Error); // Login error
+        }
+    }
+
+    public void Share()
+    {
+        Login(); // call Login()
+        //FB.ShareLink(new System.Uri("https://github.com/BrianD147"), "Check out my GitHub!", "Testing if I can pass a score of: " + score + "!");
     }
 }
